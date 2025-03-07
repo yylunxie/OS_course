dbb4cf33a36e45ed05d29add7e1d95c11e42428b
David Chen <davidchen8711@gmail.com>
2025-03-07 18:57:22 +0800

    update

--- devices/shutdown.c
+++ devices/shutdown.c
@@ -112,7 +112,7 @@ shutdown_power_off (void)
    * default 0x501).  Qemu's exit code is double the value plus one,
    * so there is no way to exit cleanly.  We use 0x31 which should
    * result in a qemu exit code of 0x63.  */
-  outw(0x604, 0x2000);
+  outb (0x501, 0x31);
 
   /* This will power off a VMware VM if "gui.exitOnCLIHLT = TRUE"
      is set in its configuration file.  (The "pintos" script does
